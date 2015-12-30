c8695f696d57d7456638b4c9ea6a7dedbe03563b
Nicholas Perkins <Nicholas.S.Perkins@gmail.com>
2015-12-30 00:36:43 -0600

    Expanded example.html capabilities.  Added property sorting, fixed a few things with click, added URL field for reading parsing remote JSON, allow hiding of json input for better text searching.

--- src/sjViewerHandlers.js
+++ src/sjViewerHandlers.js
@@ -6,7 +6,18 @@
     var showAllBtn = document.getElementById('showAll');
     var collapseAllBtn = document.getElementById('collapseAll');
     var demoBtn = document.getElementById('runDemo');
+    var urlInput = document.getElementById('url');
+    var sortBtn = document.getElementById('sort');
+    var hideBtn = document.getElementById('togglevis');
 
+    var urlField = document.getElementById('urlField');
+    var jsonField = document.getElementById('jsonField');
+
+    hideBtn.addEventListener('click', function () {
+        urlField.classList.toggle('hidden');
+        jsonField.classList.toggle('hidden');
+        this.innerHTML = this.innerHTML === 'Hide inputs'? 'Show inputs' : 'Hide inputs';
+    }, false);
 
     showAllBtn.addEventListener('click', function () {
         var hidden = document.querySelectorAll('.collapse');
@@ -24,15 +35,15 @@
     }, false);
 
 
-    function doCollapse(e) {
-        e.stopPropagation();
+    function doCollapse(element) {
         var item;
         //detect if it's the object from the dblclick, or the prop/bracket from the single click
-        if(this.classList.contains('Array') || this.classList.contains('Object')){
-            item = this;
+        if(element.classList.contains('Array') || element.classList.contains('Object')){
+            item = element;
         }else{
-            item = this.parentElement;
+            item = element.parentElement;
         }
+
 
         if(item.classList.contains('collapse')){
             //if child is opened, open all of the parents as well
@@ -63,24 +74,134 @@
 
     }
 
+    function getFromUrl(url,success){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                success(xmlhttp.responseText);
+            }else if(xmlhttp.readyState === 4){
+                alert("Error in url request");
+            }
+        };
+        var query = 'originator=simpleJsonView';
+        if(url.indexOf('?') < 0){
+            url+= '?' + query;
+        }else{
+            url+= '&' + query;
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.setRequestHeader('Accept', 'application/json, text/javascript, */*; q=0.01');
+        xmlhttp.send();
+    }
 
-    runBtn.addEventListener('click', function(){
-        containerDiv.innerHTML = sjViewer.render(textArea.value);
+    function renderJson(jsonText){
+        containerDiv.innerHTML = sjViewer.render(jsonText);
         var collapsible1 = document.querySelectorAll('.Array > .propName, .Array > .bracket, .Object > .propName, .Object > .bracket');
         var doubleClickCollapse = document.querySelectorAll('.Array, .Object');
         var j;
         for (j = 0; j < collapsible1.length; ++j) {
-            collapsible1[j].addEventListener('click', doCollapse, false);
+            collapsible1[j].addEventListener('click',function(e){
+                e.stopPropagation();
+                doCollapse(this);
+            }, false);
         }
         for (j = 0; j < doubleClickCollapse.length; ++j) {
-            doubleClickCollapse[j].addEventListener('dblclick', doCollapse, false);
+            doubleClickCollapse[j].addEventListener('dblclick', function(e){
+                e.stopPropagation();
+                if(!this.classList.contains('collapse')){ //ignore if opened, so you can click on text
+                    return;
+                }
+                doCollapse(this);
+            }, false);
         }
+    }
+
+    runBtn.addEventListener('click', function(){
+        var url = urlInput.value;
+        if(url !== ''){
+            getFromUrl(url,function(jsonText){
+                textArea.value = jsonText;
+                renderJson(jsonText);
+            });
+        }else{
+            renderJson(textArea.value);
+        }
+
     }, false);
 
     demoBtn.addEventListener('click',function(){
         textArea.value = DEMO_CODE;
         runBtn.click();
     },false);
+
+
+    sortBtn.addEventListener('click', function () {
+        var mainObject = document.querySelector('#jsonView > ul, #jsonView ol');
+        if(!mainObject){return;}
+        var pre = [];
+        var post = [];
+        var obj = null;
+
+        if(!this.dataset.state || this.dataset.state === 'desc'){
+            this.dataset.state = 'asc';
+        }else{
+            this.dataset.state = 'desc'
+        }
+
+        pre.push(mainObject);
+        while(pre.length > 0){
+            obj = pre.pop();
+
+            post.push(obj);
+
+            if(obj.children){
+                for(var j = 0; j < obj.children.length; ++j){
+                    pre.push(obj.children[j]);
+                }
+            }
+        }
+
+        while (post.length > 0) {
+            var ele = post.pop();
+            if(ele.tagName === 'UL'){
+                sortList(ele,this.dataset.state);
+            }
+        }
+
+        function sortList(ul,order){
+            var children = convertToArray(ul.children);
+            if(order === 'asc'){
+                children.sort(function(li1,li2){
+                    return li1.children[0].innerHTML.toLowerCase().localeCompare(li2.children[0].innerHTML.toLowerCase());
+                });
+            }else{
+                children.sort(function(li1,li2){
+                    return li2.children[0].innerHTML.toLowerCase().localeCompare(li1.children[0].innerHTML.toLowerCase());
+                });
+            }
+
+            //remove_children
+            while(ul.firstChild){
+                ul.removeChild(ul.firstChild);
+            }
+
+            //add sorted children
+            children.forEach(function(child){
+                ul.appendChild(child);
+            });
+        }
+
+        function convertToArray(collection){
+            var arr = [];
+            if(collection){
+                for(var k = 0; k < collection.length; ++k){
+                    arr.push(collection[k]);
+                }
+            }
+            return arr;
+        }
+    }, false);
+
 
 
 })();
